8f553bc663b85202dc628aed0e6b1396a25770c2
hwangs <shwang67@my.bcit.ca>
2025-04-01 09:46:45 -0700

    Add GitHub Actions deploy workflow

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-    base: "/",   // ✅ 무조건 이렇게!
-    plugins: [react()]
-  })
-  

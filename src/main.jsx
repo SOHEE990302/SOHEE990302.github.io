2e05b782962fb07409d8b6965fd0ebb2d04f02eb
hwangs <shwang67@my.bcit.ca>
2025-04-01 00:50:30 -0700

    Remove wrong vite.config and fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom'; // 여기 변경
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter> {/* 여기 변경 */}
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
+  <HashRouter>
+    <App />
+  </HashRouter>,
   document.getElementById('root')
 );

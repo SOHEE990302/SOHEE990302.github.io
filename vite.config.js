60de93cbd4b35583aec6f7cddf7acf4df5c8374e
hwangs <shwang67@my.bcit.ca>
2025-04-01 01:04:40 -0700

    Fix Vite base path for GitHub Pages user site

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/SOHEE990302.github.io/",  // GitHub 레포 이름
-  plugins: [react()],
-})
+    base: "/SOHEE990302.github.io/",  // 정확한 레포 이름!!!
+    plugins: [react()],
+  })
+  
